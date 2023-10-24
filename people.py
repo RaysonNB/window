be18a3a084f35c0bf306df6f69b095f153a80507
RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2023-10-24 19:10:42 +0800

    Create people.py

---
+++ people.py
@@ -0,0 +1,37 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+
+camera = cv2.VideoCapture(0)
+while camera.isOpened():
+    success, frame = camera.read()
+    if not success: break
+
+    results = model(frame)
+    '''
+    print(results[0].boxes)
+    for i in results:
+        for j in i.boxes:
+            print(j.cls,i.names[int(j.cls)])
+            for x1,y1,x2,y2 in j.xyxy:
+                x1,y1,x2,y2,cls=map(int,[x1,y1,x2,y2,cls])
+                print(cls)
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
+    '''
+    for i in results:
+        for(x1,y1,x2,y2), cls, conf in zip(i.boxes.xyxy, i.boxes.cls, i.boxes.conf):
+            x1,y1,x2,y2,cls=map(int,[x1,y1,x2,y2,cls])
+            print(cls,i.names[cls])
+            color = (0, 255, 0)
+            if cls==0:
+                color=(0,0,255)
+            cv2.rectangle(frame,(x1,y1),(x2,y2),color,2)
+
+            cv2.putText(frame,"%s %2.f" %(i.names[cls],conf),(x1,y1+20),0,0.7,color,2)
+    cv2.imshow("frame",frame)
+    key_code=cv2.waitKey(1)
+    if key_code in [27,ord('q')]:
+        break
+camera.release()
+cv2.destroyAllWindows()
